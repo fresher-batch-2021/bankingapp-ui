618dd55292b21438a46d8950778ed896ecef1aa6
Amritha M <amrithavenkat2951998@gmail.com>
2021-08-18 18:09:40 +0530

    today files are added

--- js/deposit.js
+++ js/deposit.js
@@ -9,19 +9,19 @@ const formatter = new Intl.NumberFormat('en-US', {
     the default value for minimumFractionDigits depends on the currency
     and is usually already 2
     */
-  });
+});
 
 // accept deposits from user, store deposits in array
 
-    function btnDeposits(){
-alert("hello");
-let acctBalanceLbl = document.getElementById("#acctBalanceLbl");
-// alert(acctBalanceLbl);
-const deposits = [];
+function btnDeposits() {
+    alert("hello");
+    let acctBalanceLbl = document.getElementById("#acctBalanceLbl");
+    // alert(acctBalanceLbl);
+    const deposits = [];
 
-let totalBalance = 0;
+    let totalBalance = 0;
 
-const userDeposit = document.querySelector("#userDeposit");
+    const userDeposit = document.querySelector("#userDeposit");
 
     // checks if deposit is a number
     if (isNaN(userDeposit.value)) {
@@ -29,34 +29,34 @@ const userDeposit = document.querySelector("#userDeposit");
         return userDeposit.value = '';
     } else {
 
-    // checks if deposit meets parameters
+        // checks if deposit meets parameters
         if (userDeposit.value < 0.01 || userDeposit.value > 10000) {
             alert("You can only deposit between $0.01 and $10,000.")
             return userDeposit.value = '';
         } else {
-    
-        // push deposit to array
-        deposits.push(Number(userDeposit.value));
-        // calculate Total Balance
-        totalBalance += (Number(userDeposit.value));
 
-        // format TotalBalance to show $ XX.XX (2 decimal places)
-        let totalBalanceFormatted = formatter.format(totalBalance);
-        document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+            // push deposit to array
+            deposits.push(Number(userDeposit.value));
+            // calculate Total Balance
+            totalBalance += (Number(userDeposit.value));
 
-    // print deposit to console to verify success
-    console.log("$" + userDeposit.value);
-    return userDeposit.value = '';
+            // format TotalBalance to show $ XX.XX (2 decimal places)
+            let totalBalanceFormatted = formatter.format(totalBalance);
+            document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+
+            // print deposit to console to verify success
+            console.log("$" + userDeposit.value);
+            return userDeposit.value = '';
+        }
     }
-}
-    
+
 }
 
 // accept withdrawals from user, store withdrawals in array
-function btnWithdraws(){
+function btnWithdraws() {
 
     let withdrawals = [];
-let userWithdraw = document.querySelector("#userWithdraw");
+    let userWithdraw = document.querySelector("#userWithdraw");
 
     // checks if withdrawal is a number
     if (isNaN(userWithdraw.value)) {
@@ -71,23 +71,23 @@ let userWithdraw = document.querySelector("#userWithdraw");
             return userWithdraw.value = '';
         } else {
 
-        // push withdrawal to array
-        withdrawals.push(Number(userWithdraw.value));
-        // calculate Total Balance
-        totalBalance -= (Number(userWithdraw.value));
+            // push withdrawal to array
+            withdrawals.push(Number(userWithdraw.value));
+            // calculate Total Balance
+            totalBalance -= (Number(userWithdraw.value));
 
-        // format TotalBalance to show $ XX.XX (2 decimal places)
-        let totalBalanceFormatted = formatter.format(totalBalance);
-        document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+            // format TotalBalance to show $ XX.XX (2 decimal places)
+            let totalBalanceFormatted = formatter.format(totalBalance);
+            document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
 
-    // print withdrawal to console to verfify success
-    console.log("$" + userWithdraw.value);
-    return userWithdraw.value = '';
+            // print withdrawal to console to verfify success
+            console.log("$" + userWithdraw.value);
+            return userWithdraw.value = '';
+        }
     }
-}
 }
 
 // format TotalBalance to show $ XX.XX (2 decimal places)
 
-// let totalBalanceFormatted = formatter.format(totalBalance);
-// document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
+ let totalBalanceFormatted = formatter.format(totalBalance);
+ document.getElementById("acctBalanceLbl").innerHTML = totalBalanceFormatted;
--- js/service.js
+++ js/service.js
@@ -1,7 +1,12 @@
-const dbusername = "apikey-v2-zyhv5j7i61imeby1qya0ma2ejrc0fkf9n4e4bl3w5gn";//registration url
-const dbpassword = "ec6094ae0714dc7a5ffc50a86924bef3";
+const dbusername = "apikey-v2-zyhv5j7i61imeby1qya0ma2ejrc0fkf9n4e4bl3w5gn";
 const basicAuth = " Basic " + btoa(dbusername + ":" + dbpassword);
-class userService {
+class UserService {
+    /**
+     * 
+     * @param {String} email 
+     * @param {String} password 
+     * @returns 
+     */
     static login(email, password) {
 
         const url = "https://fffdcced-9a09-44ae-aa2f-e27add7efeb7-bluemix.cloudantnosqldb.appdomain.cloud/bankingapp_user/_find";
@@ -13,8 +18,7 @@ class userService {
             },
             fields: ["name", "email"]
         };
-        console.log(requestData);//for our verification
-
+        console.log(requestData);
 
         return axios.post(url, requestData, {
             headers: { Authorization: basicAuth },
@@ -24,7 +28,7 @@ class userService {
     static register(registerObj) {
 
         const url = "https://fffdcced-9a09-44ae-aa2f-e27add7efeb7-bluemix.cloudantnosqldb.appdomain.cloud/bankingapp_user";
-        console.log(basicAuth);//for our verification
+        console.log(basicAuth);
 
 
         return axios.post(url, registerObj, {
